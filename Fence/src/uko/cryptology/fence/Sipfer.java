0c45ba7596459ad2d5296b8fc4edd4562e1f67ba
Uko <uko@unikernel.net>
2011-05-31 18:50:51 +0300

    Added freuency counter.
    Fence cipher not working yet.

---
+++ Fence/src/uko/cryptology/fence/Sipfer.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uko.cryptology.fence;
+
+
+/**
+ *
+ * @author uko
+ */
+public class Sipfer
+{
+	String code(String src,int key)
+	{
+		char[] preDst = new char[src.length()];
+		int hight=0;
+//		for(int i=0, i<src.length(), i++)
+//		{
+//			
+//		}
+		String dst=new String(preDst);
+		return dst;
+	}
+	
+}
